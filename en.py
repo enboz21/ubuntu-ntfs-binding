280754ae9b92d59a635d2dcab286c960b1d609d2
enboz21 <enesbozdag0811@gmail.com>
2024-09-05 21:53:56 +0300

    ingilizce kısmı eklendi iki tarafada açıklamalar uygulandı

---
+++ en.py
@@ -0,0 +1,48 @@
+import os
+
+# Get the username of the currently logged-in user
+user = os.getlogin()
+
+# Set the path where the connection files will be created
+path = f"/media/{user}/"
+
+def list_files(index):
+    print("Existing file names")
+    # Display existing file names to the user
+    files = os.listdir(path)
+    for file in files:
+        index += 1
+        print(f"{index}. {file}")
+    return index
+
+s = str
+continue_loop = True
+while continue_loop:
+    # Variable 'index' is used to prevent selecting excessively high values
+    index = 0
+    index = list_files(index)
+    print('Press "y" to create a new file')
+    s = input()
+    # Check if the input value is a number
+    if s.isdigit():
+        # If the input value is a number, check if it is less than or equal to the index and greater than 0
+        if int(s) <= index and int(s) > 0:
+            continue_loop = False
+    # If the input value is not a number, check if it's for creating a file
+    elif s == "y":
+        print("File name")
+        # Request the file name
+        name = input()
+        # Create the file
+        os.chdir(path)
+        os.system(f"sudo -S mkdir {name}")
+
+# List files again to select one
+files = os.listdir(path)
+# Select a file from the list
+selected_file = files[int(s) - 1]
+# Get the port information for the drive
+print("Drive port information")
+drive_port = input()
+# Prompt for the password and mount the drive with the connection code
+os.system(f"sudo -S mount -t ntfs-3g /dev/{drive_port} /media/{user}/{selected_file}")
--- tr.py
+++ tr.py
@@ -1,37 +1,48 @@
 import os
 
-# sudo mount -t ntfs-3g /dev/sdh1 /media/enboz21/dışbirim
+# Şu anda giriş yapan kullanıcının ismini alır
+kullanici = os.getlogin()
 
-dad=os.getlogin()
-yol=f"/media/{dad}/"
-def tek(i):
-    print("varolan dosya isimleri")
-    filess = os.listdir(yol)
-    for file in filess:
-        i=i+1
-        print(f"{i}. {file}")
-    return i
+# Bağlantı dosyalarının oluşturulacağı yolu ayarlar
+yol = f"/media/{kullanici}/"
 
+def dosyaları_listele(index):
+    print("Mevcut dosya isimleri")
+    # Mevcut dosya isimlerini kullanıcıya gösterir
+    dosyalar = os.listdir(yol)
+    for dosya in dosyalar:
+        index += 1
+        print(f"{index}. {dosya}")
+    return index
 
-s=str
-b=True
-while(b):
-    i=0
-    i=tek(i)
-    print('yeni dosya için "y" ye basın')
-    s=input()
-    if(s.isdigit()):
-        if int(s) <= i and int(s) > 0:
-            b = False
-
-    elif(s=="y"):
-        print("dosya ismi")
-        ad=input()
+s = str
+devam_et = True
+while devam_et:
+    # Çok yüksek bir değer seçilmesini önlemek için 'index' değişkeni kullanılır
+    index = 0
+    index = dosyaları_listele(index)
+    print('Yeni dosya oluşturmak için "y" ye basın')
+    s = input()
+    # Kullanıcıdan alınan değerin bir sayı olup olmadığı kontrol edilir
+    if s.isdigit():
+        # Eğer kullanıcıdan alınan değer bir sayıysa, bu sayının index'ten küçük ve 0'dan büyük olup olmadığı kontrol edilir
+        if int(s) <= index and int(s) > 0:
+            devam_et = False
+    # Eğer kullanıcıdan alınan değer bir sayı değilse, dosya oluşturma talimatı olarak değerlendirilir
+    elif s == "y":
+        print("Dosya ismi")
+        # Dosya ismi talep edilir
+        ad = input()
+        # Dosya oluşturulur
         os.chdir(yol)
         os.system(f"sudo -S mkdir {ad}")
-filess = os.listdir(f"/media/{dad}/")
-strf=filess[i-1]
-print("sürücünün port bilgisi")
-d=input()
-os.system(f"sudo -S mount -t ntfs-3g /dev/{d} /media/{dad}/{strf}")
 
+# Dosyaları tekrar listele ve bir dosya seç
+dosyalar = os.listdir(yol)
+# Listeden bir dosya seçilir
+secili_dosya = dosyalar[int(s) - 1]
+# Sürücünün port bilgisi alınır
+print("Sürücünün port bilgisi")
+surucu_port = input()
+# Şifre talep edilir ve bağlantı koduyla dosya eşleştirilir
+os.system(f"sudo -S mount -t ntfs-3g /dev/{surucu_port} /media/{kullanici}/{secili_dosya}")
